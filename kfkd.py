e8f355a7962a934ec5803528e94b13bc277bef79
Oleg Taykalo <oleg.taykalo@gmail.com>
2016-02-21 23:20:14 +0000

    saving stuff

--- kfkd.py
+++ kfkd.py
@@ -1,21 +1,36 @@
-import os
+"""
+To use this script, first run this to fit your first model:
 
+  python kfkd.py fit
+
+Then train a bunch of specialists that intiliaze their weights from
+your first model:
+
+  python kfkd.py fit_specialists net.pickle
+
+Plot their error curves:
+
+  python kfkd.py plot_learning_curves net-specialists.pickle
+
+And finally make predictions to submit to Kaggle:
+
+  python kfkd.py predict net-specialists.pickle
+"""
+
+import cPickle as pickle
+from datetime import datetime
+import os
+import sys
+
+from matplotlib import pyplot
 import numpy as np
+from lasagne import layers
+from nolearn.lasagne import BatchIterator
+from nolearn.lasagne import NeuralNet
 from pandas import DataFrame
 from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
-
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
-
-import matplotlib
-matplotlib.use('Agg')
-
-from matplotlib import pyplot
-import cPickle as pickle
-
-from sklearn.metrics import mean_squared_error
+import theano
 
 try:
     from lasagne.layers.cuda_convnet import Conv2DCCLayer as Conv2DLayer
@@ -24,75 +39,74 @@ except ImportError:
     Conv2DLayer = layers.Conv2DLayer
     MaxPool2DLayer = layers.MaxPool2DLayer
 
+
+sys.setrecursionlimit(10000)  # for pickle...
+np.random.seed(42)
+
 FTRAIN = '~/gamma/training.csv'
 FTEST = '~/gamma/test.csv'
 FLOOKUP = '~/gamma/idlook.csv'
 
-def load(test=False, cols = None):
-	fname = FTEST if test else FTRAIN
-	df = read_csv(os.path.expanduser(fname))
 
-	df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+def float32(k):
+    return np.cast['float32'](k)
 
-	if cols:
-		df = df[list(cols) + ['Image']]
-	print(df.count())
-	df = df.dropna()
-	
-	X = np.vstack(df['Image'].values) / 255.
-	X = X.astype(np.float32)
-	
-	if not test:
-		y = df[df.columns[:-1]].values
-		y = (y-48) /48 # scale coordinates to [-1,1]
-		X, y = shuffle(X, y, random_state = 42)
-		y = y.astype(np.float32)
-	else:
-		y = None
 
-	return X, y
+def load(test=False, cols=None):
+    """Loads data from FTEST if *test* is True, otherwise from FTRAIN.
+    Pass a list of *cols* if you're only interested in a subset of the
+    target columns.
+    """
+    fname = FTEST if test else FTRAIN
+    df = read_csv(os.path.expanduser(fname))  # load pandas dataframe
+
+    # The Image column has pixel values separated by space; convert
+    # the values to numpy arrays:
+    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+
+    if cols:  # get a subset of columns
+        df = df[list(cols) + ['Image']]
+
+    print(df.count())  # prints the number of values for each column
+    df = df.dropna()  # drop all rows that have missing values in them
+
+    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    X = X.astype(np.float32)
+
+    if not test:  # only FTRAIN has any target columns
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
+        X, y = shuffle(X, y, random_state=42)  # shuffle train data
+        y = y.astype(np.float32)
+    else:
+        y = None
+
+    return X, y
+
+
+def load2d(test=False, cols=None):
+    X, y = load(test=test, cols=cols)
+    X = X.reshape(-1, 1, 96, 96)
+    return X, y
 
 
 def plot_sample(x, y, axis):
     img = x.reshape(96, 96)
     axis.imshow(img, cmap='gray')
-    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
-
-def load2d(test=False, cols = None):
-	X, y = load(test=test)
-	X = X.reshape(-1,1,96,96)
-	return X,y
+    if y is not None:
+        axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
 
 
-def plot_train(net, name):
-	
-	train_loss = np.array([i["train_loss"] for i in net.train_history_])
-	valid_loss = np.array([i["valid_loss"] for i in net.train_history_])
-	fig = pyplot.figure(1)	
-	pyplot.plot(train_loss, linewidth=3, label=name+"_train")
-	pyplot.plot(valid_loss, linewidth=3, label=name+"_valid1")
+def plot_weights(weights):
+    fig = pyplot.figure(figsize=(6, 6))
+    fig.subplots_adjust(
+        left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
-	pyplot.grid()
-	pyplot.legend()
-	pyplot.xlabel("epoch")
-	pyplot.ylabel("loss")
-	pyplot.yscale("log")
-	pyplot.savefig(name + ".png")
-	pyplot.close()
+    for i in range(16):
+        ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+        ax.imshow(weights[:, i].reshape(96, 96), cmap='gray')
+    pyplot.show()
 
-
-def plot_faces(X, y, name):
-	fig = pyplot.figure(figsize=(6, 6))
-	fig.subplots_adjust( left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
-
-	for i in range(16):
-		ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
-		plot_sample(X[i], y[i], ax)
-
-	fig.savefig(name + "_faces.png")
-	pyplot.close()
-
-from nolearn.lasagne import BatchIterator
 
 class FlipBatchIterator(BatchIterator):
     flip_indices = [
@@ -122,11 +136,6 @@ class FlipBatchIterator(BatchIterator):
         return Xb, yb
 
 
-import theano
-
-def float32(k):
-    return np.cast['float32'](k)
-
 class AdjustVariable(object):
     def __init__(self, name, start=0.03, stop=0.001):
         self.name = name
@@ -138,8 +147,9 @@ class AdjustVariable(object):
             self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
 
         epoch = train_history[-1]['epoch']
-        new_value = float32(self.ls[epoch - 1])
+        new_value = np.cast['float32'](self.ls[epoch - 1])
         getattr(nn, self.name).set_value(new_value)
+
 
 class EarlyStopping(object):
     def __init__(self, patience=100):
@@ -162,88 +172,14 @@ class EarlyStopping(object):
             nn.load_params_from(self.best_weights)
             raise StopIteration()
 
-net6 = NeuralNet(
-    layers=[
-        ('input', layers.InputLayer),
-        ('conv1', layers.Conv2DLayer),
-        ('pool1', layers.MaxPool2DLayer),
-        ('dropout1', layers.DropoutLayer),  # !
-        ('conv2', layers.Conv2DLayer),
-        ('pool2', layers.MaxPool2DLayer),
-        ('dropout2', layers.DropoutLayer),  # !
-        ('conv3', layers.Conv2DLayer),
-        ('pool3', layers.MaxPool2DLayer),
-        ('dropout3', layers.DropoutLayer),  # !
-        ('hidden4', layers.DenseLayer),
-        ('dropout4', layers.DropoutLayer),  # !
-        ('hidden5', layers.DenseLayer),
-	('dropout5',layers.DropoutLayer),
-	('hidden6',layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    input_shape=(None, 1, 96, 96),
-    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    dropout1_p=0.1,  # !
-    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    dropout2_p=0.2,  # !
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
-    dropout3_p=0.3,  # !
-    hidden4_num_units=1000,
-    dropout4_p=0.4,  # !
-    hidden5_num_units=750,
-    dropout5_p=0.5,
-    hidden6_num_units=300,
-    output_num_units=30, output_nonlinearity=None,
-
-    update_learning_rate=theano.shared(float32(0.03)),
-    update_momentum=theano.shared(float32(0.9)),
-
-    regression=True,
-    batch_iterator_train=FlipBatchIterator(batch_size=128),
-    on_epoch_finished=[
-        AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
-        AdjustVariable('update_momentum', start=0.9, stop=0.999),
-        ],
-    max_epochs=10000,
-    verbose=1,
-    )
-
-
-net1 = NeuralNet(
-    layers=[  # three layers: one hidden layer
-        ('input', layers.InputLayer),
-        ('hidden', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    # layer parameters:
-    input_shape=(None, 9216),  # 96x96 input pixels per batch
-    hidden_num_units=100,  # number of units in hidden layer
-    output_nonlinearity=None,  # output layer uses identity function
-    output_num_units=30,  # 30 target values
-
-    # optimization method:
-    update=nesterov_momentum,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
-
-    regression=True,  # flag to indicate we're dealing with regression problem
-    max_epochs=400,  # we want to train this many epochs
-    verbose=1,
-    )
-
-
 
 net = NeuralNet(
     layers=[
         ('input', layers.InputLayer),
         ('conv1', Conv2DLayer),
-#        ('conv11', Conv2DLayer),
-#	('conv111', Conv2DLayer),
         ('pool1', MaxPool2DLayer),
         ('dropout1', layers.DropoutLayer),
         ('conv2', Conv2DLayer),
-#	('conv22', Conv2DLayer),
-#	('conv222', Conv2DLayer),
         ('pool2', MaxPool2DLayer),
         ('dropout2', layers.DropoutLayer),
         ('conv3', Conv2DLayer),
@@ -252,29 +188,18 @@ net = NeuralNet(
         ('hidden4', layers.DenseLayer),
         ('dropout4', layers.DropoutLayer),
         ('hidden5', layers.DenseLayer),
-	('dropout5', layers.DropoutLayer),
-	('hidden6', layers.DenseLayer),
         ('output', layers.DenseLayer),
         ],
     input_shape=(None, 1, 96, 96),
-    conv1_num_filters=32, conv1_filter_size=(3, 3), 
- #   conv11_num_filters=40, conv11_filter_size=(3, 3),
- #   conv111_num_filters=56, conv111_filter_size=(3, 3),
-
-    pool1_pool_size=(2, 2),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
     dropout1_p=0.1,
     conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-  #  conv22_num_filters=72, conv22_filter_size=(2, 2),
-  #  conv222_num_filters=96, conv222_filter_size=(2, 2),
-
     dropout2_p=0.2,
     conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
     dropout3_p=0.3,
     hidden4_num_units=1000,
     dropout4_p=0.5,
     hidden5_num_units=1000,
-    dropout5_p=0.5,
-    hidden6_num_units=300,
     output_num_units=30, output_nonlinearity=None,
 
     update_learning_rate=theano.shared(float32(0.03)),
@@ -287,13 +212,10 @@ net = NeuralNet(
         AdjustVariable('update_momentum', start=0.9, stop=0.999),
         EarlyStopping(patience=200),
         ],
-    max_epochs=300,
+    max_epochs=3000,
     verbose=1,
     )
 
-
-import sys
-sys.setrecursionlimit(10000)
 
 def fit():
     X, y = load2d()
@@ -378,7 +300,7 @@ def fit_specialists(fname_pretrain=None):
         model = clone(net)
         model.output_num_units = y.shape[1]
         model.batch_iterator_train.flip_indices = setting['flip_indices']
-        model.max_epochs = 20##int(4e6 / y.shape[0])
+        model.max_epochs = int(4e6 / y.shape[0])
         if 'kwargs' in setting:
             # an option 'kwargs' in the settings list may be used to
             # set any other parameter of the net:
@@ -404,11 +326,11 @@ def predict(fname_specialists='net-specialists.pickle'):
         specialists = pickle.load(f)
 
     X = load2d(test=True)[0]
-    y_pred = np.empty((X.shape[0], 0))
+    y_pred =  specialists.values()[0].predict(X)##np.empty((X.shape[0], 0))
 
     for model in specialists.values():
         y_pred1 = model.predict(X)
-        y_pred = np.hstack([y_pred, y_pred1])
+        y_pred = np.hstack([(y_pred  + y_pred1)/2])
 
     columns = ()
     for cols in specialists.keys():
@@ -484,45 +406,7 @@ def plot_learning_curves(fname_specialists='net-specialists.pickle'):
     ax.set_ylim((1.0, 4.0))
     ax.grid()
     pyplot.ylabel("RMSE")
-    pyplot.savefig('alle.png')
-
-
-#X,y = load2d()
-#net.fit(X,y)
-#net1 = pickle.load(open('../net1.pickle','rb'))
-#testX, _ = load(test=True)
-#y_pred = net.predict(testX)
-
-#	print("mean_squared for 1 "  + str(mean_squared_error(net1.predict(X),y)))
-
-# Training for 1000 epochs will take a while.  We'll pickle the
-# trained model so that we can load it back later:
-#import cPickle as pickle
-#with open('net.pickle', 'wb') as f:
-#	pickle.dump(net1, f, -1)
-
-#plot_train(net1, "net")
-#plot_faces(testX, y_pred, "net")
-
-#exit()
-#X, y = load2d()  # load 2-d data
-#net6.fit(X, y)
-
-#with open('net7.pickle','wb') as f:
-#	pickle.dump(net6, f, -1)i
-
-#net6 = pickle.load(open('../net7.pickle', 'rb'))
-
-#plot_train(net6, "net7")
-#convX,_ = load2d(test=True)
-#y_pred6 = net6.predict(convX)
-
-#with open('y_pred7.pickle', 'wb') as f:
-#	pickle.dump(y_pred6, f,-1)
-
-#plot_faces(convX, y_pred6, "net7")
-
-#print("mean_squared for 7 "  + str(mean_squared_error(net6.predict(X),y)))
+    pyplot.show()
 
 
 if __name__ == '__main__':
